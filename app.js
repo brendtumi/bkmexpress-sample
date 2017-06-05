89f4fbc31eb5f223048a9c2b3a1ae7e0de62c0ec
Tümay Çeber <tumayceber@gmail.com>
2017-06-05 12:46:26 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var bkm = require('./routes/bkm');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', bkm);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    var error = req.app.get('env') === 'development' ? err : {message: err.message};
+    res.status(err.status || 500);
+
+    setTimeout(function () {
+        console.error("app.use error", err);
+    }, 1000);
+
+    res.json(error);
+});
+
+module.exports = app;
